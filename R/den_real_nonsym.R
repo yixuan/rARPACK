393604fde6ba0f411f0fed4d1f0f8b52970e2602
Yixuan Qiu <yixuan.qiu@cos.name>
2013-03-31 10:23:14 -0400

    Clean duplicate code

--- R/den_real_nonsym.R
+++ R/den_real_nonsym.R
@@ -1,60 +1,3 @@
-eigs.den_real_nonsym <- function(A, k, which = "LM", sigma = 0.0,
-                                 opts = list())
-{
-    n = nrow(A);
-    if (n != ncol(A))
-        stop("'A' must be a square matrix");
-    if (n < 3)
-        stop("dimension of 'A' must be at least 3");
-    if (typeof(A) != "double")
-        mode(A) = "double";
-    if (k <= 0 | k >= n - 1)
-        stop("'k' must satisfy 0 < k < nrow(A) - 1.\nTo calculate all eigenvalues, try eigen()");
-    
-    arpack.param = list(which = "LM",
-                        ncv = min(n - 1, max(2 * k + 1, 20)),
-                        tol = 1e-8,
-                        maxitr = 300,
-                        sigmar = 0.0,
-                        sigmai = 0.0);
-    eigenv.type = c("LM", "SM", "LR", "SR", "LI", "SI");
-    if (inherits(sigma, "character"))
-    {
-        if (!(sigma %in% eigenv.type))
-            stop(sprintf("when type of 'sigma' is character, it must one of\n%s",
-                         paste(eigenv.type, collapse = ", ")));
-        arpack.param$which = sigma;
-    } else if (inherits(sigma, "numeric")){
-        arpack.param$sigmar = Re(sigma[1]);
-        arpack.param$sigmai = Im(sigma[1]);
-    } else {
-        stop("'sigma' must be either of the class 'character' or 'numeric'");
-    }
-    arpack.param[names(opts)] = opts;
-    if (arpack.param$ncv < k + 2 | arpack.param$ncv > n)
-        stop("'opts$ncv' must be >= k+2 and <= nrow(A)");
-    
-    res = .Call("den_real_nonsym", A, as.integer(n), as.integer(k),
-                as.character(arpack.param$which), as.integer(arpack.param$ncv),
-                as.numeric(arpack.param$tol), as.integer(arpack.param$maxitr),
-                as.numeric(arpack.param$sigmar), as.numeric(arpack.param$sigmai),
-                PACKAGE = "rarpack");
-    
-    if (!is.null(res$values))
-    {
-        res$values = res$values[1:res$nconv];
-        return(res);
-    } else {
-        res$values = res$d.real[1:res$nconv] + res$d.imag[1:res$nconv] * 1i;
-        res$vectors = res$v.real + res$v.imag * 1i;
-        res$d.real = NULL;
-        res$d.imag = NULL;
-        res$v.real = NULL;
-        res$v.imag = NULL;
-    }
-    return(res);
-}
-
 ##' @rdname eigs-methods
 ##' @aliases eigs-matrix,numeric,character,numeric,list-method
 setMethod("eigs", signature(A = "matrix",
@@ -62,7 +5,9 @@ setMethod("eigs", signature(A = "matrix",
                             which = "character",
                             sigma = "numeric",
                             opts = "list"),
-          eigs.den_real_nonsym);
+          function(A, k, which, sigma, opts, ...)
+              eigs.real_nonsym(A, k, which, sigma, opts, ...,
+                               mattype = "dense"));
 ##' @rdname eigs-methods
 ##' @aliases eigs-matrix,numeric,character,numeric,missing-method
 setMethod("eigs", signature(A = "matrix",
@@ -71,7 +16,8 @@ setMethod("eigs", signature(A = "matrix",
                             sigma = "numeric",
                             opts = "missing"),
           function(A, k, which, sigma, ...)
-              eigs.den_real_nonsym(A, k, which, sigma, list(), ...));
+              eigs.real_nonsym(A, k, which, sigma, list(), ...,
+                               mattype = "dense"));
 ##' @rdname eigs-methods
 ##' @aliases eigs-matrix,numeric,character,missing,missing-method
 setMethod("eigs", signature(A = "matrix",
@@ -80,7 +26,8 @@ setMethod("eigs", signature(A = "matrix",
                             sigma = "missing",
                             opts = "missing"),
           function(A, k, which, ...)
-              eigs.den_real_nonsym(A, k, which, 0.0, list(), ...));
+              eigs.real_nonsym(A, k, which, 0.0, list(), ...,
+                               mattype = "dense"));
 ##' @rdname eigs-methods
 ##' @aliases eigs-matrix,numeric,missing,missing,missing-method
 setMethod("eigs", signature(A = "matrix",
@@ -89,4 +36,5 @@ setMethod("eigs", signature(A = "matrix",
                             sigma = "missing",
                             opts = "missing"),
           function(A, k, which, ...)
-              eigs.den_real_nonsym(A, k, "LM", 0.0, list(), ...));
+              eigs.real_nonsym(A, k, "LM", 0.0, list(), ...,
+                               mattype = "dense"));
--- R/sparse_real_nonsym.R
+++ R/sparse_real_nonsym.R
@@ -1,58 +1,3 @@
-eigs.sparse_real_nonsym <- function(A, k, which = "LM", sigma = 0.0,
-                               opts = list())
-{
-    n = nrow(A);
-    if (n != ncol(A))
-        stop("'A' must be a square matrix");
-    if (n < 3)
-        stop("dimension of 'A' must be at least 3");
-    if (k <= 0 | k >= n - 1)
-        stop("'k' must satisfy 0 < k < nrow(A) - 1.\nTo calculate all eigenvalues, try eigen()");
-    
-    arpack.param = list(which = "LM",
-                        ncv = min(n - 1, max(2 * k + 1, 20)),
-                        tol = 1e-8,
-                        maxitr = 300,
-                        sigmar = 0.0,
-                        sigmai = 0.0);
-    eigenv.type = c("LM", "SM", "LR", "SR", "LI", "SI");
-    if (inherits(sigma, "character"))
-    {
-        if (!(sigma %in% eigenv.type))
-            stop(sprintf("when type of 'sigma' is character, it must one of\n%s",
-                         paste(eigenv.type, collapse = ", ")));
-        arpack.param$which = sigma;
-    } else if (inherits(sigma, "numeric")){
-        arpack.param$sigmar = Re(sigma[1]);
-        arpack.param$sigmai = Im(sigma[1]);
-    } else {
-        stop("'sigma' must be either of the class 'character' or 'numeric'");
-    }
-    arpack.param[names(opts)] = opts;
-    if (arpack.param$ncv < k + 2 | arpack.param$ncv > n)
-        stop("'opts$ncv' must be >= k+2 and <= nrow(A)");
-    
-    res = .Call("sparse_real_nonsym", A, as.integer(n), as.integer(k),
-                as.character(arpack.param$which), as.integer(arpack.param$ncv),
-                as.numeric(arpack.param$tol), as.integer(arpack.param$maxitr),
-                as.numeric(arpack.param$sigmar), as.numeric(arpack.param$sigmai),
-                PACKAGE = "rarpack");
-    
-    if (!is.null(res$values))
-    {
-        res$values = res$values[1:res$nconv];
-        return(res);
-    } else {
-        res$values = res$d.real[1:res$nconv] + res$d.imag[1:res$nconv] * 1i;
-        res$vectors = res$v.real + res$v.imag * 1i;
-        res$d.real = NULL;
-        res$d.imag = NULL;
-        res$v.real = NULL;
-        res$v.imag = NULL;
-    }
-    return(res);
-}
-
 ##' @rdname eigs-methods
 ##' @aliases eigs-dgCMatrix,numeric,character,numeric,list-method
 setMethod("eigs", signature(A = "dgCMatrix",
@@ -60,7 +5,9 @@ setMethod("eigs", signature(A = "dgCMatrix",
                             which = "character",
                             sigma = "numeric",
                             opts = "list"),
-          eigs.sparse_real_nonsym);
+          function(A, k, which, sigma, opts, ...)
+              eigs.real_nonsym(A, k, which, sigma, opts, ...,
+                               mattype = "sparse"));
 ##' @rdname eigs-methods
 ##' @aliases eigs-dgCMatrix,numeric,character,numeric,missing-method
 setMethod("eigs", signature(A = "dgCMatrix",
@@ -69,7 +16,8 @@ setMethod("eigs", signature(A = "dgCMatrix",
                             sigma = "numeric",
                             opts = "missing"),
           function(A, k, which, sigma, ...)
-              eigs.sparse_real_nonsym(A, k, which, sigma, list(), ...));
+              eigs.real_nonsym(A, k, which, sigma, list(), ...,
+                               mattype = "sparse"));
 ##' @rdname eigs-methods
 ##' @aliases eigs-dgCMatrix,numeric,character,missing,missing-method
 setMethod("eigs", signature(A = "dgCMatrix",
@@ -78,7 +26,8 @@ setMethod("eigs", signature(A = "dgCMatrix",
                             sigma = "missing",
                             opts = "missing"),
           function(A, k, which, ...)
-              eigs.sparse_real_nonsym(A, k, which, 0.0, list(), ...));
+              eigs.real_nonsym(A, k, which, 0.0, list(), ...,
+                               mattype = "sparse"));
 ##' @rdname eigs-methods
 ##' @aliases eigs-dgCMatrix,numeric,missing,missing,missing-method
 setMethod("eigs", signature(A = "dgCMatrix",
@@ -87,4 +36,5 @@ setMethod("eigs", signature(A = "dgCMatrix",
                             sigma = "missing",
                             opts = "missing"),
           function(A, k, which, ...)
-              eigs.sparse_real_nonsym(A, k, "LM", 0.0, list(), ...));
+              eigs.real_nonsym(A, k, "LM", 0.0, list(), ...,
+                               mattype = "sparse"));
