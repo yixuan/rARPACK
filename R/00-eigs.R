83451dc5cc2b027879bbff0a8d50e6623a4add36
Yixuan Qiu <yixuan.qiu@cos.name>
2013-07-19 23:36:30 -0400

    documentation for eigs.sym()

--- R/00-eigs.R
+++ R/00-eigs.R
@@ -1,11 +1,24 @@
 ##' Find a few eigenvalues and eigenvectors for square matrix
 ##'
+##' @description
 ##' Given an \code{n} by \code{n} matrix \code{A},
-##' this function can calculate a limited
-##' number of eigenvalues and eigenvectors of \code{A}. Users can specify
-##' the selection criteria by argument \code{which},
-##' e.g., choosing the \code{k} largest or smallest
+##' function \code{eigs()} can calculate a limited
+##' number of eigenvalues and eigenvectors of \code{A}.
+##' Users can specify the selection criteria by argument
+##' \code{which}, e.g., choosing the \code{k} largest or smallest
 ##' eigenvalues and the corresponding eigenvectors.
+##' 
+##' Currently \code{eigs()} supports matrices of class "matrix",
+##' "dgCMatrix" and "dsyMatrix".
+##' 
+##' \code{eigs.sym()} assumes the matrix is symmetric,
+##' and only the lower triangle (or upper triangle, which is
+##' controlled by the argument \code{lower}) is used for
+##' computation, which in general reduces the workload.
+##' Notice that \code{eigs.sym()} only applies to "ordinary" matrix,
+##' i.e., of class "matrix". If you want to calculate
+##' eigen values/vectors of matrix of "dsyMatrix" class, use
+##' \code{eigs()} instead.
 ##' 
 ##' @param A The matrix whose eigen values/vectors are to be computed.
 ##' @param k Number of eigenvalues requested.
@@ -14,6 +27,8 @@
 ##' @param opts Control parameters related to the computing
 ##' algorithm. See Details below.
 ##' @param \dots Currently not used.
+##' @param lower For symmetric matrices, should the lower triangle
+##'              or upper triangle be used. 
 ##'
 ##' @details The \code{which} argument is a character string
 ##' that specifies the type of eigenvalues to be computed.
@@ -36,14 +51,13 @@
 ##' \item{\code{tol}}{Precision parameter. Default is 1e-8.}
 ##' \item{\code{maxitr}}{Maximum number of iterations. Default is 300.}
 ##' \item{\code{retvec}}{Whether to compute eigenvectors. If FALSE,
-##'                      only returning eigenvalues.}
+##'                      only calculate and return eigenvalues.}
 ##' }
 ##' 
 ##' @return A list of converged eigenvalues and eigenvectors.
 ##' \item{nconv}{Number of converged eigenvalues.}
 ##' \item{values}{Computed eigenvalues.}
 ##' \item{vectors}{Computed eigenvectors. \code{vectors[, j]} corresponds to \code{values[j]}.}
-##' @note Currently only real nonsymmetric matrices are supported.
 ##' @author Yixuan Qiu <\url{http://statr.me}>
 ##' @seealso \code{\link[base]{eigen}()}, \code{\link[base]{svd}()}
 ##'
@@ -60,6 +74,7 @@
 ##' ## since A2 is symmetric
 ##' A2 = crossprod(A1);
 ##' eigs(A2, k);
+##' eigs.sym(A2, k);
 eigs <- function(A, k, which = "LM", sigma = 0.0, opts = list(), ...)
 {
     if(inherits(A, "matrix"))
