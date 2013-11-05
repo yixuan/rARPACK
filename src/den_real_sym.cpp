dd5c5134b6f42d41f3b19ac9fb484410836db219
Yixuan Qiu <yixuan.qiu@cos.name>
2013-11-05 16:23:53 -0500

    cannot use Cholesky decomposition, use LU instead

--- src/den_real_sym.cpp
+++ src/den_real_sym.cpp
@@ -4,9 +4,7 @@
 using Eigen::Map;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
-using Eigen::LDLT;
-using Eigen::Lower;
-using Eigen::Upper;
+using Eigen::PartialPivLU;
 typedef Eigen::Map<Eigen::VectorXd> MapVec;
 
 
@@ -26,40 +24,23 @@ void den_sym_mat_v_prod(SEXP mat, double *x_in, double *y_out,
 }
 
 
-// Data passed to den_sym*_mat_v_prod_shinv()
-// L and U represent different solver using lower and upper triangles
+// Data passed to den_sym_mat_v_prod_shinv()
 typedef struct {
-    const LDLT<MatrixXd> *solver;
+    const PartialPivLU<MatrixXd> *solver;
     MapVec *x_vec;
     MapVec *y_vec;
-} SymLData;
-
-typedef struct {
-    const LDLT<MatrixXd, Upper> *solver;
-    MapVec *x_vec;
-    MapVec *y_vec;
-} SymUData;
+} SymData;
 
 // Shift-and-invert mode
-void den_syml_mat_v_prod_shinv(SEXP mat, double *x_in, double *y_out,
+void den_sym_mat_v_prod_shinv(SEXP mat, double *x_in, double *y_out,
                     int n, void *data)
 {
-    SymLData *sldata = (SymLData *) data;
+    SymData *sdata = (SymData *) data;
     // First map x_in and y_out to x_vec and y_vec respectively,
     // and then solve the linear equation Mat * y_out = x_in
-    new (sldata->x_vec) MapVec(x_in, n);
-    new (sldata->y_vec) MapVec(y_out, n);
-    *(sldata->y_vec) = (*(sldata->solver)).solve(*(sldata->x_vec));
-}
-void den_symu_mat_v_prod_shinv(SEXP mat, double *x_in, double *y_out,
-                    int n, void *data)
-{
-    SymUData *sudata = (SymUData *) data;
-    // First map x_in and y_out to x_vec and y_vec respectively,
-    // and then solve the linear equation Mat * y_out = x_in
-    new (sudata->x_vec) MapVec(x_in, n);
-    new (sudata->y_vec) MapVec(y_out, n);
-    *(sudata->y_vec) = (*(sudata->solver)).solve(*(sudata->x_vec));
+    new (sdata->x_vec) MapVec(x_in, n);
+    new (sdata->y_vec) MapVec(y_out, n);
+    *(sdata->y_vec) = (*(sdata->solver)).solve(*(sdata->x_vec));
 }
 
 
@@ -93,56 +74,32 @@ BEGIN_RCPP
             A(i, i) -= REAL(sigma_scalar_r)[0];
         }
         
+        // LU decomposition
+        const PartialPivLU<MatrixXd> luA(A);
+        
+        // Change A back
+        for(int i = 0; i < n; i++)
+        {
+            A(i, i) += REAL(sigma_scalar_r)[0];
+        }
+        
         // Declare Eigen vectors (hey here I mean the C++ library Eigen,
         // not eigenvectors) that will be connected to workd in the iteration
         MapVec x_vec(NULL, n);
         MapVec y_vec(NULL, n);
         
-        // If we use lower triangle of A
-        if(LOGICAL(lower_logical_r)[0])
-        {
-            // Choleskey decomposition
-            const LDLT<MatrixXd> cholA(A);
-            // Data passed to den_syml_mat_v_prod_shinv()
-            SymLData data;
-            data.solver = &cholA;
-            data.x_vec = &x_vec;
-            data.y_vec = &y_vec;
-            
-            // Change A back
-            for(int i = 0; i < n; i++)
-            {
-                A(i, i) += REAL(sigma_scalar_r)[0];
-            }
-            
-            return do_eigs_sym(A_mat_r, n_scalar_r, k_scalar_r,
+        // Data passed to den_sym_mat_v_prod_shinv()
+        SymData data;
+        data.solver = &luA;
+        data.x_vec = &x_vec;
+        data.y_vec = &y_vec;
+              
+        return do_eigs_sym(A_mat_r, n_scalar_r, k_scalar_r,
                        which_string_r, ncv_scalar_r,
                        tol_scalar_r, maxitr_scalar_r,
                        retvec_logical_r, sigma_scalar_r,
                        workmode_scalar_r,
-                       den_syml_mat_v_prod_shinv, &data);
-        } else {
-            // Choleskey decomposition
-            const LDLT<MatrixXd, Upper> cholA(A);
-            // Data passed to den_symu_mat_v_prod_shinv()
-            SymUData data;
-            data.solver = &cholA;
-            data.x_vec = &x_vec;
-            data.y_vec = &y_vec;
-            
-            // Change A back
-            for(int i = 0; i < n; i++)
-            {
-                A(i, i) += REAL(sigma_scalar_r)[0];
-            }
-
-            return do_eigs_sym(A_mat_r, n_scalar_r, k_scalar_r,
-                       which_string_r, ncv_scalar_r,
-                       tol_scalar_r, maxitr_scalar_r,
-                       retvec_logical_r, sigma_scalar_r,
-                       workmode_scalar_r,
-                       den_symu_mat_v_prod_shinv, &data);
-        }
+                       den_sym_mat_v_prod_shinv, &data);
     }
     
     // Should not get here
