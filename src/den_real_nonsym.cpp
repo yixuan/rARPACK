51213ff1d506b323cab93ad3a66ed131fac7ce56
Yixuan Qiu <yixuan.qiu@cos.name>
2013-11-25 10:04:32 -0500

    improved documentation

--- src/den_real_nonsym.cpp
+++ src/den_real_nonsym.cpp
@@ -2,16 +2,18 @@
 #include "do_eigs.h"
 
 using Rcpp::as;
-using Eigen::Map;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using Eigen::MatrixXcd;
 using Eigen::VectorXcd;
 using Eigen::PartialPivLU;
+
 typedef Eigen::Map<Eigen::VectorXd> MapVec;
+typedef Eigen::Map<Eigen::MatrixXd> MapMat;
 
 
 // Dense matrix-vector product
+// This function uses BLAS to calculte y_out = Mat * x_in
 void den_mat_v_prod(SEXP mat, double *x_in, double *y_out,
                     int n, void *data)
 {
@@ -32,40 +34,46 @@ typedef struct {
     const PartialPivLU<MatrixXd> *solver;
     MapVec *x_vec;
     MapVec *y_vec;
-} DenRData;
+} DenRLUData; // dense, real, LU decomposition
 
+// Data passed to denc_mat_v_prod_shinv()
 typedef struct {
     const PartialPivLU<MatrixXcd> *solver;
     VectorXcd *x_vec;
     MapVec *y_vec;
-} DenCData;
+} DenCLUData; // dense, complex, LU decomposition
 
 
-// Shift-and-invert mode
+// The two functions below support the Shift-and-invert mode of ARPACK
+// They use LU decomposition to solve the linear equation
+//                      Mat * y_out = x_in
+// Equivalent to calculate y_out = Inv(Mat) * x_in
+// denc_mat_v_prod_shinv() calculates y_out = Re(Inv(Mat)) * x_in
 void denr_mat_v_prod_shinv(SEXP mat, double *x_in, double *y_out,
-                    int n, void *data)
+                           int n, void *data)
 {
-    DenRData *drdata = (DenRData *) data;
+    DenRLUData *drludata = (DenRLUData *) data;
     // First map x_in and y_out to x_vec and y_vec respectively,
     // and then solve the linear equation Mat * y_out = x_in
-    new (drdata->x_vec) MapVec(x_in, n);
-    new (drdata->y_vec) MapVec(y_out, n);
-    *(drdata->y_vec) = (*(drdata->solver)).solve(*(drdata->x_vec));
+    new (drludata->x_vec) MapVec(x_in, n);
+    new (drludata->y_vec) MapVec(y_out, n);
+    (*(drludata->y_vec)).noalias() = (*(drludata->solver)).solve(*(drludata->x_vec));
 }
 
 void denc_mat_v_prod_shinv(SEXP mat, double *x_in, double *y_out,
-                    int n, void *data)
+                           int n, void *data)
 {
-    DenCData *dcdata = (DenCData *) data;
+    DenCLUData *dcludata = (DenCLUData *) data;
     // First map x_in and y_out to x_vec and y_vec respectively,
     // and then solve the linear equation Mat * y_out = x_in
-    (*(dcdata->x_vec)).real() = MapVec(x_in, n);
-    new (dcdata->y_vec) MapVec(y_out, n);
-    *(dcdata->y_vec) = (*(dcdata->solver)).solve(*(dcdata->x_vec)).real();
+    (*(dcludata->x_vec)).real() = MapVec(x_in, n);
+    new (dcludata->y_vec) MapVec(y_out, n);
+    (*(dcludata->y_vec)).noalias() = (*(dcludata->solver)).solve(*(dcludata->x_vec)).real();
 }
 
 
 
+// Main function to solve dense, real, nonsymmetric eigen problems
 RcppExport SEXP den_real_nonsym(SEXP A_mat_r, SEXP n_scalar_r, SEXP k_scalar_r,
                                 SEXP params_list_r,
                                 SEXP sigmareal_logical_r)
@@ -75,9 +83,9 @@ BEGIN_RCPP
     Rcpp::List params_rcpp(params_list_r);
     int workmode = as<int>(params_rcpp["workmode"]);
     
-    // If we don't need shift-and-invert mode
     if(workmode == 1)
     {
+        // If we don't need shift-and-invert mode
         return do_eigs_nonsym(A_mat_r, n_scalar_r, k_scalar_r,
                               params_list_r,  
                               den_mat_v_prod, NULL);
@@ -89,8 +97,10 @@ BEGIN_RCPP
         
         if(LOGICAL(sigmareal_logical_r)[0])
         {
+            // If sigma is real
+            
             // Map A_mat_r to Eigen matrix
-            Map<MatrixXd> A(REAL(A_mat_r), n, n);
+            MapMat A(REAL(A_mat_r), n, n);
         
             // Subtract the diagonal elements by sigma, i.e., A - sigma * I
             for(int i = 0; i < n; i++)
@@ -107,13 +117,14 @@ BEGIN_RCPP
                 A(i, i) += sigmar;
             }
         
-            // Declare Eigen vectors (hey here I mean the C++ library Eigen,
-            // not eigenvectors) that will be connected to workd in the iteration
+            // Declare Eigen vectors
+            // (hey here I mean the C++ library Eigen, not eigenvectors)
+            // that will be connected to ARPACK
             MapVec x_vec(NULL, n);
             MapVec y_vec(NULL, n);
             
             // Data passed to denr_mat_v_prod_shinv()
-            DenRData data;
+            DenRLUData data;
             data.solver = &luA;
             data.x_vec = &x_vec;
             data.y_vec = &y_vec;
@@ -121,9 +132,11 @@ BEGIN_RCPP
             return do_eigs_nonsym(A_mat_r, n_scalar_r, k_scalar_r,
                                   params_list_r,
                                   denr_mat_v_prod_shinv, &data);
-        } else { 
+        } else {
+            // If sigma is complex
+            
             // Map A_mat_r to Eigen matrix
-            Map<MatrixXd> AR(REAL(A_mat_r), n, n);
+            MapMat AR(REAL(A_mat_r), n, n);
             
             //MatrixXcd A(n, n);
             //A.real() = AR;
@@ -139,14 +152,13 @@ BEGIN_RCPP
             // LU decomposition
             const PartialPivLU<MatrixXcd> luA(A);
         
-            // Declare Eigen vectors (hey here I mean the C++ library Eigen,
-            // not eigenvectors) that will be connected to workd in the iteration
+            // Declare Eigen vectors that will be connected to ARPACK
             VectorXcd x_vec(n);
-            x_vec.imag().setZero();
+            x_vec.setZero();
             MapVec y_vec(NULL, n);
             
             // Data passed to denc_mat_v_prod_shinv()
-            DenCData data;
+            DenCLUData data;
             data.solver = &luA;
             data.x_vec = &x_vec;
             data.y_vec = &y_vec;
@@ -168,6 +180,11 @@ END_RCPP
 
 
 
+
+
+
+
+
 // Alternative methods
 //
 //
@@ -178,22 +195,22 @@ typedef struct {
     const PartialPivLU<MatrixXd> *solverS;
     MapVec *x_vec;
     MapVec *y_vec;
-} DenCData2;
+} DenCLUData2;
 
 // Shift-and-invert mode
 void denc_mat_v_prod_shinv2(SEXP mat, double *x_in, double *y_out,
                             int n, void *data)
 {
-    DenCData2 *dcdata = (DenCData2 *) data;
+    DenCLUData2 *dcludata = (DenCLUData2 *) data;
     // First map x_in and y_out to x_vec and y_vec respectively,
     // and then solve the linear equation Mat * y_out = x_in
-    new (dcdata->x_vec) MapVec(x_in, n);
-    new (dcdata->y_vec) MapVec(y_out, n);
-    *(dcdata->y_vec) = (*(dcdata->solverA)).solve(*(dcdata->x_vec)) -
-        (*(dcdata->solverS)).solve(*(dcdata->x_vec));
+    new (dcludata->x_vec) MapVec(x_in, n);
+    new (dcludata->y_vec) MapVec(y_out, n);
+    (*(dcludata->y_vec)).noalias() = (*(dcludata->solverA)).solve(*(dcludata->x_vec)) -
+        (*(dcludata->solverS)).solve(*(dcludata->x_vec));
 }
 
-// Using real matrix operations to calculate Re(Inv(M))
+// Use real matrix operations to calculate Re(Inv(M))
 // May be faster than den_real_nonsym() in standard optimization flags,
 // but seems slower when -msse4 is used.
 RcppExport SEXP den_real_nonsym2(SEXP A_mat_r, SEXP n_scalar_r, SEXP k_scalar_r,
@@ -205,9 +222,9 @@ BEGIN_RCPP
     Rcpp::List params_rcpp(params_list_r);
     int workmode = as<int>(params_rcpp["workmode"]);
     
-    // If we don't need shift-and-invert mode
     if(workmode == 1)
     {
+        // If we don't need shift-and-invert mode
         return do_eigs_nonsym(A_mat_r, n_scalar_r, k_scalar_r,
                               params_list_r,  
                               den_mat_v_prod, NULL);
@@ -219,8 +236,10 @@ BEGIN_RCPP
         
         if(LOGICAL(sigmareal_logical_r)[0])
         {
+            // If sigma is real
+            
             // Map A_mat_r to Eigen matrix
-            Map<MatrixXd> A(REAL(A_mat_r), n, n);
+            MapMat A(REAL(A_mat_r), n, n);
         
             // Subtract the diagonal elements by sigma, i.e., A - sigma * I
             for(int i = 0; i < n; i++)
@@ -237,13 +256,14 @@ BEGIN_RCPP
                 A(i, i) += sigmar;
             }
         
-            // Declare Eigen vectors (hey here I mean the C++ library Eigen,
-            // not eigenvectors) that will be connected to workd in the iteration
+            // Declare Eigen vectors
+            // (hey here I mean the C++ library Eigen, not eigenvectors)
+            // that will be connected to ARPACK
             MapVec x_vec(NULL, n);
             MapVec y_vec(NULL, n);
             
             // Data passed to denr_mat_v_prod_shinv()
-            DenRData data;
+            DenRLUData data;
             data.solver = &luA;
             data.x_vec = &x_vec;
             data.y_vec = &y_vec;
@@ -252,8 +272,10 @@ BEGIN_RCPP
                                   params_list_r,
                                   denr_mat_v_prod_shinv, &data);
         } else {
+            // If sigma is complex
+            
             // Map A_mat_r to Eigen matrix
-            Map<MatrixXd> A(REAL(A_mat_r), n, n);
+            MapMat A(REAL(A_mat_r), n, n);
         
             // Subtract the diagonal elements by sigma, i.e., A - sigma * I
             for(int i = 0; i < n; i++)
@@ -291,13 +313,12 @@ BEGIN_RCPP
                 A(i, i) += sigmar;
             }
         
-            // Declare Eigen vectors (hey here I mean the C++ library Eigen,
-            // not eigenvectors) that will be connected to workd in the iteration
+            // Declare Eigen vectors that will be connected to ARPACK
             MapVec x_vec(NULL, n);
             MapVec y_vec(NULL, n);
             
             // Data passed to denc_mat_v_prod_shinv()
-            DenCData2 data;
+            DenCLUData2 data;
             data.solverA = &luA;
             data.solverS = &luS;
             data.x_vec = &x_vec;
@@ -326,9 +347,9 @@ BEGIN_RCPP
     Rcpp::List params_rcpp(params_list_r);
     int workmode = as<int>(params_rcpp["workmode"]);
     
-    // If we don't need shift-and-invert mode
     if(workmode == 1)
     {
+        // If we don't need shift-and-invert mode
         return do_eigs_nonsym(A_mat_r, n_scalar_r, k_scalar_r,
                               params_list_r,  
                               den_mat_v_prod, NULL);
@@ -340,8 +361,10 @@ BEGIN_RCPP
         
         if(LOGICAL(sigmareal_logical_r)[0])
         {
+            // If sigma is real
+            
             // Map A_mat_r to Eigen matrix
-            Map<MatrixXd> A(REAL(A_mat_r), n, n);
+            MapMat A(REAL(A_mat_r), n, n);
         
             // Subtract the diagonal elements by sigma, i.e., A - sigma * I
             for(int i = 0; i < n; i++)
@@ -358,13 +381,14 @@ BEGIN_RCPP
                 A(i, i) += sigmar;
             }
         
-            // Declare Eigen vectors (hey here I mean the C++ library Eigen,
-            // not eigenvectors) that will be connected to workd in the iteration
+            // Declare Eigen vectors
+            // (hey here I mean the C++ library Eigen, not eigenvectors)
+            // that will be connected to ARPACK
             MapVec x_vec(NULL, n);
             MapVec y_vec(NULL, n);
             
             // Data passed to denr_mat_v_prod_shinv()
-            DenRData data;
+            DenRLUData data;
             data.solver = &luA;
             data.x_vec = &x_vec;
             data.y_vec = &y_vec;
@@ -373,8 +397,10 @@ BEGIN_RCPP
                                   params_list_r,
                                   denr_mat_v_prod_shinv, &data);
         } else {
+            // If sigma is complex
+            
             // Map A_mat_r to Eigen matrix
-            Map<MatrixXd> A(REAL(A_mat_r), n, n);
+            MapMat A(REAL(A_mat_r), n, n);
         
             // Subtract the diagonal elements by sigma, i.e., A - sigma * I
             for(int i = 0; i < n; i++)
@@ -392,6 +418,7 @@ BEGIN_RCPP
             // S <- S * A
             double b2 = sigmai * sigmai;
             MatrixXd S(A);
+            
             // Use BLAS
             // C <- alpha * A * B + beta * C
             // F77_CALL(dgemm)(&trans, &trans, &n, &n, &n,
@@ -423,13 +450,12 @@ BEGIN_RCPP
                 A(i, i) += sigmar;
             }
         
-            // Declare Eigen vectors (hey here I mean the C++ library Eigen,
-            // not eigenvectors) that will be connected to workd in the iteration
+            // Declare Eigen vectors that will be connected to ARPACK
             MapVec x_vec(NULL, n);
             MapVec y_vec(NULL, n);
             
             // Data passed to denc_mat_v_prod_shinv()
-            DenCData2 data;
+            DenCLUData2 data;
             data.solverA = &luA;
             data.solverS = &luS;
             data.x_vec = &x_vec;
--- src/sparse_real_nonsym.cpp
+++ src/sparse_real_nonsym.cpp
@@ -6,32 +6,38 @@ using Eigen::VectorXi;
 using Eigen::MatrixXcd;
 using Eigen::VectorXcd;
 using Eigen::SparseLU;
+
 typedef Eigen::SparseMatrix<double> SpMat;
 typedef Eigen::SparseMatrix< std::complex<double> > SpCMat;
 typedef Eigen::MappedSparseMatrix<double> MapSpMat;
 typedef Eigen::Map<Eigen::VectorXd> MapVec;
 typedef Eigen::Map<Eigen::MatrixXd> MapMat;
 
-// Data passed to matrix-vector product function
+// Data passed to sparse_mat_v_prod()
 typedef struct {
     const MapSpMat *A;
     MapVec *x_vec;
     MapVec *y_vec;
 } SparseData;
 
+// Data passed to sparser_mat_v_prod_shinv()
 typedef struct {
     SparseLU<SpMat> *solver;
     MapVec *x_vec;
     MapVec *y_vec;
 } SparseRLUData; // sparse, real, LU decomposition
 
+// Data passed to sparsec_mat_v_prod_shinv()
 typedef struct {
     SparseLU<SpCMat> *solver;
     VectorXcd *x_vec;
     MapVec *y_vec;
 } SparseCLUData; // sparse, complex, LU decomposition
 
+
 // Sparse matrix-vector product
+// This function calcultes y_out = Mat * x_in
+// where Mat is a sparse matrix
 void sparse_mat_v_prod(SEXP mat, double *x_in, double *y_out,
                        int n, void *data)
 {
@@ -43,6 +49,12 @@ void sparse_mat_v_prod(SEXP mat, double *x_in, double *y_out,
     (*(spdata->y_vec)).noalias() = *(spdata->A) * *(spdata->x_vec);
 }
 
+// The two functions below support the Shift-and-invert mode of ARPACK
+// They use sparse LU decomposition to solve the linear equation
+//                      Mat * y_out = x_in
+// where Mat is a sparse matrix
+// Equivalent to calculate y_out = Inv(Mat) * x_in
+// sparsec_mat_v_prod_shinv() calculates y_out = Re(Inv(Mat)) * x_in
 void sparser_mat_v_prod_shinv(SEXP mat, double *x_in, double *y_out,
                               int n, void *data)
 {  
@@ -51,7 +63,7 @@ void sparser_mat_v_prod_shinv(SEXP mat, double *x_in, double *y_out,
     // and then solve the linear equation Mat * y_out = x_in
     new (sprludata->x_vec) MapVec(x_in, n);
     new (sprludata->y_vec) MapVec(y_out, n);
-    *(sprludata->y_vec) = (*(sprludata->solver)).solve(*(sprludata->x_vec));
+    (*(sprludata->y_vec)).noalias() = (*(sprludata->solver)).solve(*(sprludata->x_vec));
 }
 
 void sparsec_mat_v_prod_shinv(SEXP mat, double *x_in, double *y_out,
@@ -62,10 +74,12 @@ void sparsec_mat_v_prod_shinv(SEXP mat, double *x_in, double *y_out,
     // and then solve the linear equation Mat * y_out = x_in
     (*(spcludata->x_vec)).real() = MapVec(x_in, n);
     new (spcludata->y_vec) MapVec(y_out, n);
-    *(spcludata->y_vec) = (*(spcludata->solver)).solve(*(spcludata->x_vec)).real();
+    (*(spcludata->y_vec)).noalias() = (*(spcludata->solver)).solve(*(spcludata->x_vec)).real();
 }
 
 
+
+// Main function to solve sparse, real, nonsymmetric eigen problems
 RcppExport SEXP sparse_real_nonsym(SEXP A_mat_r, SEXP n_scalar_r, SEXP k_scalar_r,
                                    SEXP params_list_r,
                                    SEXP sigmareal_logical_r)
@@ -76,13 +90,16 @@ BEGIN_RCPP
     int workmode = as<int>(params_rcpp["workmode"]);
     int n = INTEGER(n_scalar_r)[0];
     
-    // If we don't need shift-and-invert mode
     if(workmode == 1)
     {
+        // If we don't need shift-and-invert mode
+        
         // Map A_mat_r to Eigen sparse matrix
         const MapSpMat A(as<MapSpMat>(A_mat_r));
-        // Declare Eigen vectors (hey here I mean the C++ library Eigen,
-        // not eigenvectors) that will be connected to workd in the iteration
+  
+        // Declare Eigen vectors
+        // (hey here I mean the C++ library Eigen, not eigenvectors)
+        // that will be connected to ARPACK
         MapVec x_vec(NULL, n);
         MapVec y_vec(NULL, n);
     
@@ -96,13 +113,16 @@ BEGIN_RCPP
                               params_list_r,
                               sparse_mat_v_prod, &data);
     } else {
+        
         double sigmar = as<double>(params_rcpp["sigmar"]);
         double sigmai = as<double>(params_rcpp["sigmai"]);
         int n = INTEGER(n_scalar_r)[0];
         
         if(LOGICAL(sigmareal_logical_r)[0])
         {
-            // Map A_mat_r to Eigen matrix
+            // If sigma is real
+            
+            // Map A_mat_r to Eigen sparse matrix
             const MapSpMat A(as<MapSpMat>(A_mat_r));
         
             // Create a sparse identity matrix
@@ -113,11 +133,10 @@ BEGIN_RCPP
             SparseLU<SpMat> luA;
             luA.compute(A - sigmar * I);
             if(luA.info() != Eigen::Success) {
-                ::Rf_error("Eigen: decomposition of A - sigma * I failed");
+                ::Rf_error("Eigen: LU decomposition of A - sigma * I failed");
             }
 
-            // Declare Eigen vectors (hey here I mean the C++ library Eigen,
-            // not eigenvectors) that will be connected to workd in the iteration
+            // Declare Eigen vectors that will be connected to ARPACK
             MapVec x_vec(NULL, n);
             MapVec y_vec(NULL, n);
             
@@ -131,6 +150,8 @@ BEGIN_RCPP
                                   params_list_r,
                                   sparser_mat_v_prod_shinv, &data);
         } else {
+            // If sigma is complex
+            
             // Map A_mat_r to Eigen matrix
             const MapSpMat AR(as<MapSpMat>(A_mat_r));
             
@@ -144,13 +165,12 @@ BEGIN_RCPP
             SparseLU<SpCMat> luA;
             luA.compute(A - std::complex<double>(sigmar, sigmai) * I);
             if(luA.info() != Eigen::Success) {
-                ::Rf_error("Eigen: decomposition of A - sigma * I failed");
+                ::Rf_error("Eigen: LU decomposition of A - sigma * I failed");
             }
         
-            // Declare Eigen vectors (hey here I mean the C++ library Eigen,
-            // not eigenvectors) that will be connected to workd in the iteration
+            // Declare Eigen vectors that will be connected to ARPACK
             VectorXcd x_vec(n);
-            x_vec.imag().setZero();
+            x_vec.setZero();
             MapVec y_vec(NULL, n);
             
             // Data passed to sparsec_mat_v_prod_shinv()
