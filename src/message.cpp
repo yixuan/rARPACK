fdc32b629049e211ed54925c4a16d38d7c954f6b
Yixuan Qiu <yixuan.qiu@cos.name>
2014-03-14 23:32:13 -0400

    use Rcpp::stop() to handle error

--- src/message.cpp
+++ src/message.cpp
@@ -1,4 +1,5 @@
 #include "do_eigs.h"
+using std::string;
 
 // Warning and error information
 // See ARPACK/dsaupd.f for details
@@ -19,47 +20,51 @@ void dsaupd_warn(int info)
 
 void dsaupd_error(int info)
 {
+    char num[10];
+    sprintf(num, "%d", info);
+    string info_str(num);
+    
     switch(info)
     {
         case -1:
-            ::Rf_error("ARPACK/dsaupd: n must be positive");
+            Rcpp::stop("ARPACK/dsaupd: n must be positive");
             break;
         case -2:
-            ::Rf_error("ARPACK/dsaupd: k must be positive");
+            Rcpp::stop("ARPACK/dsaupd: k must be positive");
             break;
         case -3:
-            ::Rf_error("ARPACK/dsaupd: k < ncv <= n");
+            Rcpp::stop("ARPACK/dsaupd: k < ncv <= n");
             break;
         case -4:
-            ::Rf_error("ARPACK/dsaupd: maxitr must be positive");
+            Rcpp::stop("ARPACK/dsaupd: maxitr must be positive");
             break;
         case -5:
-            ::Rf_error("ARPACK/dsaupd: which must be one of 'LM', 'SM', 'LA', 'SA', 'BE'");
+            Rcpp::stop("ARPACK/dsaupd: which must be one of 'LM', 'SM', 'LA', 'SA', 'BE'");
             break;
         case -6:
-            ::Rf_error("ARPACK/dsaupd: error code %d", info);
+            Rcpp::stop("ARPACK/dsaupd: error code " + info_str);
             break;
         case -7:
-            ::Rf_error("ARPACK/dsaupd: length of private work array WORKL is not sufficient");
+            Rcpp::stop("ARPACK/dsaupd: length of private work array WORKL is not sufficient");
             break;
         case -8:
-            ::Rf_error("ARPACK/dsaupd: error return from trid. eigenvalue calculation\n"
+            Rcpp::stop("ARPACK/dsaupd: error return from trid. eigenvalue calculation\n"
                        "informational error from LAPACK routine dsteqr");
             break;
         case -9:
-            ::Rf_error("ARPACK/dsaupd: starting vector is zero");
+            Rcpp::stop("ARPACK/dsaupd: starting vector is zero");
             break;
         case -10:
         case -11:
         case -12:
         case -13:
-            ::Rf_error("ARPACK/dsaupd: error code %d", info);
+            Rcpp::stop("ARPACK/dsaupd: error code " + info_str);
             break;
         case -9999:
-            ::Rf_error("ARPACK/dsaupd: couldn't build an Arnoldi factorization");
+            Rcpp::stop("ARPACK/dsaupd: couldn't build an Arnoldi factorization");
             break;
         default:
-            ::Rf_error("ARPACK/dsaupd: error code %d", info);
+            Rcpp::stop("ARPACK/dsaupd: error code " + info_str);
             break;
     }
 }
@@ -68,49 +73,53 @@ void dsaupd_error(int info)
 // See ARPACK/dseupd.f for details
 void dseupd_error(int info)
 {
+    char num[10];
+    sprintf(num, "%d", info);
+    string info_str(num);
+
     switch(info)
     {
         case -1:
-            ::Rf_error("ARPACK/dseupd: n must be positive");
+            Rcpp::stop("ARPACK/dseupd: n must be positive");
             break;
         case -2:
-            ::Rf_error("ARPACK/dseupd: k must be positive");
+            Rcpp::stop("ARPACK/dseupd: k must be positive");
             break;
         case -3:
-            ::Rf_error("ARPACK/dseupd: k < ncv <= n");
+            Rcpp::stop("ARPACK/dseupd: k < ncv <= n");
             break;
         case -5:
-            ::Rf_error("ARPACK/dseupd: which must be one of 'LM', 'SM', 'LA', 'SA', 'BE'");
+            Rcpp::stop("ARPACK/dseupd: which must be one of 'LM', 'SM', 'LA', 'SA', 'BE'");
             break;
         case -6:
-            ::Rf_error("ARPACK/dseupd: error code %d", info);
+            Rcpp::stop("ARPACK/dseupd: error code " + info_str);
             break;
         case -7:
-            ::Rf_error("ARPACK/dseupd: length of private work WORKL array is not sufficient");
+            Rcpp::stop("ARPACK/dseupd: length of private work WORKL array is not sufficient");
             break;
         case -8:
-            ::Rf_error("ARPACK/dseupd: error return from trid. eigenvalue calculation\n"
+            Rcpp::stop("ARPACK/dseupd: error return from trid. eigenvalue calculation\n"
                        "informational error from LAPACK routine dsteqr");
             break;
         case -9:
-            ::Rf_error("ARPACK/dseupd: starting vector is zero");
+            Rcpp::stop("ARPACK/dseupd: starting vector is zero");
             break;
         case -10:
         case -11:
         case -12:
-            ::Rf_error("ARPACK/dseupd: error code %d", info);
+            Rcpp::stop("ARPACK/dseupd: error code " + info_str);
             break;
         case -14:
-            ::Rf_error("ARPACK/dseupd: DSAUPD did not find any eigenvalues to sufficient accuracy");
+            Rcpp::stop("ARPACK/dseupd: DSAUPD did not find any eigenvalues to sufficient accuracy");
             break;
         case -15:
         case -16:
-            ::Rf_error("ARPACK/dseupd: error code %d", info);
+            Rcpp::stop("ARPACK/dseupd: error code " + info_str);
         case -17:
-            ::Rf_error("ARPACK/dseupd: DSEUPD got a different count of the number of converged Ritz values than DSAUPD got");
+            Rcpp::stop("ARPACK/dseupd: DSEUPD got a different count of the number of converged Ritz values than DSAUPD got");
             break;
         default:
-            ::Rf_error("ARPACK/dseupd: error code %d", info);
+            Rcpp::stop("ARPACK/dseupd: error code " + info_str);
             break;
     }
 }
@@ -136,45 +145,49 @@ void dnaupd_warn(int info)
 
 void dnaupd_error(int info)
 {
+    char num[10];
+    sprintf(num, "%d", info);
+    string info_str(num);
+
     switch(info)
     {
         case -1:
-            ::Rf_error("ARPACK/dnaupd: n must be positive");
+            Rcpp::stop("ARPACK/dnaupd: n must be positive");
             break;
         case -2:
-            ::Rf_error("ARPACK/dnaupd: k must be positive");
+            Rcpp::stop("ARPACK/dnaupd: k must be positive");
             break;
         case -3:
-            ::Rf_error("ARPACK/dnaupd: 2 <= ncv - k <= n");
+            Rcpp::stop("ARPACK/dnaupd: 2 <= ncv - k <= n");
             break;
         case -4:
-            ::Rf_error("ARPACK/dnaupd: maxitr must be positive");
+            Rcpp::stop("ARPACK/dnaupd: maxitr must be positive");
             break;
         case -5:
-            ::Rf_error("ARPACK/dnaupd: which must be one of 'LM', 'SM', 'LR', 'SR', 'LI', 'SI'");
+            Rcpp::stop("ARPACK/dnaupd: which must be one of 'LM', 'SM', 'LR', 'SR', 'LI', 'SI'");
             break;
         case -6:
-            ::Rf_error("ARPACK/dnaupd: error code %d", info);
+            Rcpp::stop("ARPACK/dnaupd: error code " + info_str);
             break;
         case -7:
-            ::Rf_error("ARPACK/dnaupd: length of private work array is not sufficient");
+            Rcpp::stop("ARPACK/dnaupd: length of private work array is not sufficient");
             break;
         case -8:
-            ::Rf_error("ARPACK/dnaupd: error return from LAPACK eigenvalue calculation");
+            Rcpp::stop("ARPACK/dnaupd: error return from LAPACK eigenvalue calculation");
             break;
         case -9:
-            ::Rf_error("ARPACK/dnaupd: starting vector is zero");
+            Rcpp::stop("ARPACK/dnaupd: starting vector is zero");
             break;
         case -10:
         case -11:
         case -12:
-            ::Rf_error("ARPACK/dnaupd: error code %d", info);
+            Rcpp::stop("ARPACK/dnaupd: error code " + info_str);
             break;
         case -9999:
-            ::Rf_error("ARPACK/dnaupd: couldn't build an Arnoldi factorization");
+            Rcpp::stop("ARPACK/dnaupd: couldn't build an Arnoldi factorization");
             break;
         default:
-            ::Rf_error("ARPACK/dnaupd: error code %d", info);
+            Rcpp::stop("ARPACK/dnaupd: error code " + info_str);
             break;
     }
 }
@@ -194,45 +207,49 @@ void dneupd_warn(int info)
 
 void dneupd_error(int info)
 {
+    char num[10];
+    sprintf(num, "%d", info);
+    string info_str(num);
+
     switch(info)
     {
         case -1:
-            ::Rf_error("ARPACK/dneupd: n must be positive");
+            Rcpp::stop("ARPACK/dneupd: n must be positive");
             break;
         case -2:
-            ::Rf_error("ARPACK/dneupd: k must be positive");
+            Rcpp::stop("ARPACK/dneupd: k must be positive");
             break;
         case -3:
-            ::Rf_error("ARPACK/dneupd: 2 <= ncv - k <= n");
+            Rcpp::stop("ARPACK/dneupd: 2 <= ncv - k <= n");
             break;
         case -5:
-            ::Rf_error("ARPACK/dneupd: which must be one of 'LM', 'SM', 'LR', 'SR', 'LI', 'SI'");
+            Rcpp::stop("ARPACK/dneupd: which must be one of 'LM', 'SM', 'LR', 'SR', 'LI', 'SI'");
             break;
         case -6:
-            ::Rf_error("ARPACK/dneupd: error code %d", info);
+            Rcpp::stop("ARPACK/dneupd: error code " + info_str);
             break;
         case -7:
-            ::Rf_error("ARPACK/dneupd: length of private work WORKL array is not sufficient");
+            Rcpp::stop("ARPACK/dneupd: length of private work WORKL array is not sufficient");
             break;
         case -8:
-            ::Rf_error("ARPACK/dneupd: error return from calculation of a real Schur form");
+            Rcpp::stop("ARPACK/dneupd: error return from calculation of a real Schur form");
             break;
         case -9:
-            ::Rf_error("ARPACK/dneupd: error return from calculation of eigenvectors");
+            Rcpp::stop("ARPACK/dneupd: error return from calculation of eigenvectors");
             break;
         case -10:
         case -11:
         case -12:
         case -13:
-            ::Rf_error("ARPACK/dneupd: error code %d", info);
+            Rcpp::stop("ARPACK/dneupd: error code " + info_str);
             break;
         case -14:
-            ::Rf_error("ARPACK/dneupd: DNAUPD did not find any eigenvalues to sufficient accuracy");
+            Rcpp::stop("ARPACK/dneupd: DNAUPD did not find any eigenvalues to sufficient accuracy");
         case -15:
-            ::Rf_error("ARPACK/dneupd: DNEUPD got a different count of the number of converged Ritz values than DNAUPD got");
+            Rcpp::stop("ARPACK/dneupd: DNEUPD got a different count of the number of converged Ritz values than DNAUPD got");
             break;
         default:
-            ::Rf_error("ARPACK/dneupd: error code %d", info);
+            Rcpp::stop("ARPACK/dneupd: error code " + info_str);
             break;
     }
 }
