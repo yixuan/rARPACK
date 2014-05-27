fc9887be5fefaf4f507d731a29d15b3203ab68e5
yixuan <yixuan.qiu@cos.name>
2014-05-27 22:49:05 +0800

    add MatOp_dsyMatrix class

---
+++ src/MatOp_dsyMatrix.h
@@ -0,0 +1,27 @@
+#ifndef MATOPDSYMATRIX_H
+#define MATOPDSYMATRIX_H
+
+#include "MatOp_symmatrix.h"
+
+inline char get_uplo(SEXP dsyMatrix)
+{
+    SEXP str = GET_SLOT(dsyMatrix, Rf_install("uplo"));
+    return CHAR(STRING_ELT(str, 0))[0];
+}
+
+class MatOp_dsyMatrix : public MatOp_symmatrix
+{
+public:
+    // Constructor
+    MatOp_dsyMatrix(SEXP mat_, double sigma_ = 0,
+                    bool needSolve_ = false):
+        MatOp_symmatrix(GET_SLOT(mat_, Rf_install("x")),
+                        get_uplo(mat_),
+                        sigma_,
+                        needSolve_)
+    {}
+    virtual ~MatOp_dsyMatrix() {}
+};
+
+
+#endif // MATOPDSYMATRIX_H
