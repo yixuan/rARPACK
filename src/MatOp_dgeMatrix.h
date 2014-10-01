f7ea1665bc83e49dc6f8b64907f9c456ff166815
Yixuan Qiu <yixuan.qiu@cos.name>
2014-10-01 13:56:40 -0400

    add m_ and n_ arguments in constructor

--- src/MatOp_dgeMatrix.h
+++ src/MatOp_dgeMatrix.h
@@ -1,5 +1,5 @@
-#ifndef MATOPDGEMATRIX_H
-#define MATOPDGEMATRIX_H
+#ifndef MATOP_DGEMATRIX_H
+#define MATOP_DGEMATRIX_H
 
 #include "MatOp_matrix.h"
 
@@ -8,15 +8,14 @@ class MatOp_dgeMatrix : public MatOp_matrix
 {
 public:
     // Constructor
-    MatOp_dgeMatrix(SEXP mat_, double sigmar_ = 0, double sigmai_ = 0,
+    MatOp_dgeMatrix(SEXP mat_, int m_, int n_,
+                    double sigmar_ = 0, double sigmai_ = 0,
                     bool needSolve_ = false) :
         MatOp_matrix(REAL(GET_SLOT(mat_, Rf_install("x"))),
-                     INTEGER(GET_SLOT(mat_, Rf_install("Dim")))[0],
-                     INTEGER(GET_SLOT(mat_, Rf_install("Dim")))[1],
-                     sigmar_, sigmai_, needSolve_)
+                     m_, n_, sigmar_, sigmai_, needSolve_)
     {}
     virtual ~MatOp_dgeMatrix() {}
 };
 
 
-#endif // MATOPDGEMATRIX_H
+#endif // MATOP_DGEMATRIX_H
