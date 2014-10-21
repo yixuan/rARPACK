45aefc0c9c16c74e5d6ddf8e79ddb3816423ae65
Yixuan Qiu <yixuan.qiu@cos.name>
2014-10-20 20:49:29 -0400

    new way to implement svds_sym()

--- src/svds_funs.cpp
+++ src/svds_funs.cpp
@@ -2,13 +2,10 @@
 #include "ARPACK.h"
 #include "EigsSym.h"
 #include "MatTypes.h"
+#include "SVDsSym.h"
 
 using Rcpp::as;
 
-
-SEXP do_svds_sym(MatOp *op, SEXP n_scalar_r,
-                 SEXP k_scalar_r, SEXP nu_scalar_r, SEXP nv_scalar_r,
-                 SEXP params_list_r);
 
 SEXP do_svds_gen(MatOp *op, SEXP m_scalar_r, SEXP n_scalar_r,
                  SEXP k_scalar_r, SEXP nu_scalar_r, SEXP nv_scalar_r,
@@ -22,23 +19,23 @@ RcppExport SEXP svds_sym(SEXP A_mat_r, SEXP n_scalar_r, SEXP k_scalar_r,
                          SEXP params_list_r, SEXP mattype_scalar_r)
 {
 BEGIN_RCPP
+   
+    Rcpp::List params_svds(params_list_r);
+    
+    int n = as<int>(n_scalar_r);
+    int k = as<int>(k_scalar_r);
+    int nu = as<int>(nu_scalar_r);
+    int nv = as<int>(nv_scalar_r);
+    int ncv = as<int>(params_svds["ncv"]);
+    double tol = as<double>(params_svds["tol"]);
+    int maxitr = as<int>(params_svds["maxitr"]);
+    bool rvec = (nu > 0) || (nv > 0);
 
-    MatOp *op = NULL;
-    switch(as<int>(mattype_scalar_r))
-    {
-        case (int) DSYMATRIX:
-            op = new MatOp_dsyMatrix(A_mat_r,
-                                     as<int>(n_scalar_r),
-                                     MatOp_dsyMatrix::get_uplo(A_mat_r),
-                                     0, false);
-            break;
-        default:
-            Rcpp::stop("unsupported matrix type in svds()");
-    }
-
-    SEXP res = do_svds_sym(op, n_scalar_r, k_scalar_r,
-                           nu_scalar_r, nv_scalar_r,
-                           params_list_r);
+    MatOp *op = newMatOp(A_mat_r, as<int>(mattype_scalar_r), n, n);
+    
+    SVDsSym svd(n, k, nu, nv, ncv, op, tol, maxitr);
+    svd.compute(rvec);
+    SEXP res = svd.extract();
 
     delete op;
 
