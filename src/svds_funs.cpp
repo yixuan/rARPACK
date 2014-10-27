3643d4a301ddfb9dd25da66cfd4e70732a0d7db2
Yixuan Qiu <yixuan.qiu@cos.name>
2014-10-27 10:44:34 -0400

    use new implementation of svds_gen()

--- src/svds_funs.cpp
+++ src/svds_funs.cpp
@@ -1,14 +1,9 @@
 #include <Rcpp.h>
 #include "MatTypes.h"
 #include "SVDsSym.h"
+#include "SVDsGen.h"
 
 using Rcpp::as;
-
-
-SEXP do_svds_gen(MatOp *op, SEXP m_scalar_r, SEXP n_scalar_r,
-                 SEXP k_scalar_r, SEXP nu_scalar_r, SEXP nv_scalar_r,
-                 SEXP params_list_r);
-
 
 // Main function to calculate truncated SVD for
 // dense, real, symmetric matrices
@@ -49,13 +44,30 @@ RcppExport SEXP svds_gen(SEXP A_mat_r, SEXP m_scalar_r, SEXP n_scalar_r,
 {
 BEGIN_RCPP
 
+    Rcpp::List params_svds(params_list_r);
+    
     int m = as<int>(m_scalar_r);
     int n = as<int>(n_scalar_r);
-    MatOp *op = newMatOp(A_mat_r, as<int>(mattype_scalar_r), m, n);
+    int k = as<int>(k_scalar_r);
+    int nu = as<int>(nu_scalar_r);
+    int nv = as<int>(nv_scalar_r);
+    int ncv = as<int>(params_svds["ncv"]);
+    double tol = as<double>(params_svds["tol"]);
+    int maxitr = as<int>(params_svds["maxitr"]);
 
-    SEXP res = do_svds_gen(op, m_scalar_r, n_scalar_r, k_scalar_r,
-                           nu_scalar_r, nv_scalar_r,
-                           params_list_r);
+    MatOp *op = newMatOp(A_mat_r, as<int>(mattype_scalar_r), m, n);
+    
+    Rcpp::RObject res;
+    if(m > n)
+    {
+        SVDsGenTall svd(m, n, k, nu, nv, ncv, op, tol, maxitr);
+        svd.compute();
+        res = svd.extract();
+    } else {
+        SVDsGenWide svd(m, n, k, nu, nv, ncv, op, tol, maxitr);
+        svd.compute();
+        res = svd.extract();
+    }
 
     delete op;
 
