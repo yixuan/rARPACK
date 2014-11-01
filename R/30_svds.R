195a4cdfbd2df61247c78affe3b18145a7e4ccd3
Yixuan Qiu <yixuan.qiu@cos.name>
2014-11-01 18:23:19 -0400

    more examples

--- R/30_svds.R
+++ R/30_svds.R
@@ -68,15 +68,24 @@
 ##' @rdname svds
 ##' @keywords array
 ##' @examples
-##' m = 100;
-##' n = 20;
-##' k = 5;
-##' set.seed(111);
-##' A = matrix(rnorm(m * n), m);
-##' svds(A, k);
-##' svds(A, k, nu = 0, nv = 3);
+##' m = 100
+##' n = 20
+##' k = 5
+##' set.seed(111)
+##' A = matrix(rnorm(m * n), m)
+##' 
+##' svds(A, k)
+##' svds(t(A), k, nu = 0, nv = 3)
+##' 
+##' ## Sparse matrices
+##' library(Matrix)
+##' A[sample(m * n, m * n / 2)] = 0
+##' Asp1 = as(A, "dgCMatrix")
+##' Asp2 = as(A, "dgRMatrix")
+##' 
+##' svds(Asp1, k)
+##' svds(Asp2, k, nu = 0, nv = 0)
 ##'
-##' ### more examples in examples/svds.R ###
 svds <- function(A, k, nu = k, nv = k, opts = list(), ...)
     UseMethod("svds");
 
