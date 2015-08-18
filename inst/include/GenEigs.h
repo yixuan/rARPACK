9ed923fe4aa364e694b51c54dac1bb046bfa651a
Yixuan Qiu <yixuan.qiu@cos.name>
2015-08-18 13:21:57 -0400

    wrapper header

---
+++ inst/include/GenEigs.h
@@ -0,0 +1,8 @@
+#ifndef GENEIGS_H
+#define GENEIGS_H
+
+#ifdef __cplusplus
+#include <arpack_eigen/GenEigsSolver.h>
+#endif
+
+#endif /* GENEIGS_H */
---
+++ inst/include/SymEigs.h
@@ -0,0 +1,8 @@
+#ifndef SYMEIGS_H
+#define SYMEIGS_H
+
+#ifdef __cplusplus
+#include <arpack_eigen/SymEigsSolver.h>
+#endif
+
+#endif /* SYMEIGS_H */
