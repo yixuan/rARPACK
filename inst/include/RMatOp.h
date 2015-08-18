d4483f7616e767c727156315dcd84278844edddb
Yixuan Qiu <yixuan.qiu@cos.name>
2015-08-18 13:31:31 -0400

    R matrix operation headers

--- inst/include/RMatOp.h
+++ inst/include/RMatOp.h
@@ -1,26 +1,30 @@
-#ifndef MATOPS_H
-#define MATOPS_H
+#ifndef RMATOP_H
+#define RMATOP_H
 
-#include "MatOp/MatProd.h"
-#include "MatOp/MatProd_matrix.h"
-#include "MatOp/MatProd_symmatrix.h"
-#include "MatOp/MatProd_dgeMatrix.h"
-#include "MatOp/MatProd_dsyMatrix.h"
-#include "MatOp/MatProd_sparseMatrix.h"
-#include "MatOp/MatProd_function.h"
+#ifdef __cplusplus
 
-#include "MatOp/RealShift.h"
-#include "MatOp/RealShift_matrix.h"
-#include "MatOp/RealShift_symmatrix.h"
-#include "MatOp/RealShift_dgeMatrix.h"
-#include "MatOp/RealShift_dsyMatrix.h"
-#include "MatOp/RealShift_sparseMatrix.h"
+#include "RMatOp/MatProd.h"
+#include "RMatOp/MatProd_matrix.h"
+#include "RMatOp/MatProd_symmatrix.h"
+#include "RMatOp/MatProd_dgeMatrix.h"
+#include "RMatOp/MatProd_dsyMatrix.h"
+#include "RMatOp/MatProd_sparseMatrix.h"
+#include "RMatOp/MatProd_function.h"
 
-#include "MatOp/ComplexShift.h"
-#include "MatOp/ComplexShift_matrix.h"
-#include "MatOp/ComplexShift_dgeMatrix.h"
-#include "MatOp/ComplexShift_sparseMatrix.h"
+#include "RMatOp/RealShift.h"
+#include "RMatOp/RealShift_matrix.h"
+#include "RMatOp/RealShift_symmatrix.h"
+#include "RMatOp/RealShift_dgeMatrix.h"
+#include "RMatOp/RealShift_dsyMatrix.h"
+#include "RMatOp/RealShift_sparseMatrix.h"
 
-#include "MatOp/SVDOp.h"
+#include "RMatOp/ComplexShift.h"
+#include "RMatOp/ComplexShift_matrix.h"
+#include "RMatOp/ComplexShift_dgeMatrix.h"
+#include "RMatOp/ComplexShift_sparseMatrix.h"
 
-#endif // MATOPS_H
+#include "RMatOp/SVDOp.h"
+
+#endif
+
+#endif /* RMATOP_H */
