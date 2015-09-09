34200e9287a7402701e072c34ccccc103bebb8db
Yixuan Qiu <yixuan.qiu@cos.name>
2015-09-09 13:20:26 -0400

    fix wrong class name

--- inst/include/RMatOp/MatProd_dsyMatrix.h
+++ inst/include/RMatOp/MatProd_dsyMatrix.h
@@ -3,13 +3,13 @@
 
 #include <Rcpp.h>
 #include <Rdefines.h>  // for R macros
-#include "MatProd_symmatrix.h"
+#include "MatProd_sym_matrix.h"
 
-class MatProd_dsyMatrix : public MatProd_symmatrix
+class MatProd_dsyMatrix : public MatProd_sym_matrix
 {
 public:
     MatProd_dsyMatrix(SEXP mat_, const int nrow_, const char uplo_ = 'L') :
-        MatProd_symmatrix(GET_SLOT(mat_, Rf_install("x")), nrow_, uplo_)
+        MatProd_sym_matrix(GET_SLOT(mat_, Rf_install("x")), nrow_, uplo_)
     {}
 };
 
--- inst/include/RMatOp/RealShift_dsyMatrix.h
+++ inst/include/RMatOp/RealShift_dsyMatrix.h
@@ -3,13 +3,13 @@
 
 #include <Rcpp.h>
 #include <Rdefines.h>  // for R macros
-#include "RealShift_symmatrix.h"
+#include "RealShift_sym_matrix.h"
 
-class RealShift_dsyMatrix : public RealShift_symmatrix
+class RealShift_dsyMatrix : public RealShift_sym_matrix
 {
 public:
     RealShift_dsyMatrix(SEXP mat_, const int nrow_, const char uplo_ = 'L') :
-        RealShift_symmatrix(GET_SLOT(mat_, Rf_install("x")), nrow_, uplo_)
+        RealShift_sym_matrix(GET_SLOT(mat_, Rf_install("x")), nrow_, uplo_)
     {}
 };
 
