a1a090d7e7ae9342dc69a5b6aa5fc1fce5c9eeed
Yixuan Qiu <yixuan.qiu@cos.name>
2016-03-07 16:03:40 -0500

    unused source files

--- inst/include/GenEigs.h
+++
@@ -1,8 +0,0 @@
-#ifndef GENEIGS_H
-#define GENEIGS_H
-
-#ifdef __cplusplus
-#include <Spectra/GenEigsSolver.h>
-#endif
-
-#endif /* GENEIGS_H */
--- inst/include/SymEigs.h
+++
@@ -1,8 +0,0 @@
-#ifndef SYMEIGS_H
-#define SYMEIGS_H
-
-#ifdef __cplusplus
-#include <Spectra/SymEigsSolver.h>
-#endif
-
-#endif /* SYMEIGS_H */
